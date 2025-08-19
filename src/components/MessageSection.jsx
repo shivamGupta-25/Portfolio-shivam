23ad33bd91d1a36cf431312cb659c8dba76613ee
Shivam Raj Gupta <guptashivam25oct@gmail.com>
2025-08-20 01:15:28 +0530

    Improved Message Section and Added Custom Cursor

--- src/components/MessageSection.jsx
+++ src/components/MessageSection.jsx
@@ -16,9 +16,8 @@ const MessageSection = () => {
       type: "words"
     });
 
-    const paragraphSplit = SplitText.create(".message-content p", {
-      type: "words, lines",
-      linesClass: "paragraph-line"
+    const middleMsgSplit = SplitText.create(".msg-text-scroll", {
+      type: "words"
     });
 
     gsap.to(firstMsgSplit.words, {
@@ -33,6 +32,20 @@ const MessageSection = () => {
         scrub: true
       }
     });
+
+    gsap.to(middleMsgSplit.words, {
+      color: isDarkMode ? '#f1f5f9' : '#1e293b',
+      ease: "power1.in",
+      stagger: 1,
+      scrollTrigger: {
+        trigger: ".msg-text-scroll",
+        start: "30% center",
+        // markers: true,
+        end: "101% center",
+        scrub: true
+      }
+    });
+
     gsap.to(secMsgSplit.words, {
       color: isDarkMode ? '#f1f5f9' : '#1e293b',
       ease: "power1.in",
@@ -46,36 +59,6 @@ const MessageSection = () => {
       }
     });
 
-    const revealTl = gsap.timeline({
-      delay: 1,
-      scrollTrigger: {
-        trigger: ".msg-text-scroll",
-        start: "center center",
-        end: "bottom center",
-        // markers: true,
-        scrub: true
-      }
-    })
-    revealTl.to(".msg-text-scroll", {
-      duration: 1,
-      clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
-      ease: "circ.inOut"
-    });
-
-
-    const paragraphTl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".message-content p",
-        start: "top center"
-      }
-    });
-    paragraphTl.from(paragraphSplit.words, {
-      yPercent: 300,
-      rotate: 3,
-      ease: "power1.inOut",
-      duration: 1,
-      stagger: 0.01
-    })
   }, [isDarkMode]);
 
   return (
@@ -84,15 +67,10 @@ const MessageSection = () => {
         <div className='w-full h-full'>
           <div className='msg-wrapper'>
             <h1 className='first-message'>Turning Data</h1>
-
-            <div style={{
-              clipPath: 'polygon(0 0, 0 0, 0 100%, 0 100%)',
-            }} className='msg-text-scroll'>
-              <div className='md:pb-5 pb-3 px-5'>
-                <h2 className='text-slate-700 dark:text-slate-100'>into</h2>
-              </div>
+            <div className='msg-text-scroll'>
+              <h2 className='sm:mt-12'>into</h2>
             </div>
-            <h1 className='second-message'>Actionable Insights</h1>
+            <h1 className='second-message pt-8'>Actionable Insights</h1>
           </div>
         </div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './components/ui/theme-provider'
+import { SmoothCursor } from "@/components/ui/smooth-cursor";
 
 createRoot(document.getElementById('root')).render(
   <ThemeProvider>
+    <SmoothCursor />
     <App />
   </ThemeProvider>
 )
